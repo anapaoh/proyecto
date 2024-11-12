11875053c941494550d147693409fe1011ebdb5d
anapaoh <a01276728@tec.mx>
2024-11-12 13:39:24 -0600

    Create registro.h

---
+++ registro.h
@@ -0,0 +1,18 @@
+#ifndef REGISTRO_H
+#define REGISTRO_H
+
+#include <string>
+
+struct Registro {
+    std::string blanco;
+    std::string iso3;
+    std::string year;
+    std::string le_f;
+    std::string country_sp;
+    std::string fem_pop;
+    std::string mmr;
+    std::string gpi;
+    std::string femicides_number;
+};
+
+#endif
