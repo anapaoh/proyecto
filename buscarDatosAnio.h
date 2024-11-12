a865b68bc1f71b5d1b0ef05d11409db33fff9eba
anapaoh <a01276728@tec.mx>
2024-11-12 13:43:38 -0600

    Create buscarDatosAnio.h

---
+++ buscarDatosAnio.h
@@ -0,0 +1,10 @@
+#ifndef BUSCARDATOSANIO_H
+#define BUSCARDATOSANIO_H
+
+#include <string>
+#include <vector>
+#include "registro.h"  
+
+void buscarDatosAnio(const std::string& archivo, const std::string& pais, const std::string& anio);
+
+#endif
